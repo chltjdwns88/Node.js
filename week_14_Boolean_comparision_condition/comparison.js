880725bc28d606f4fe76edbeb54cd46833d2dcd1
choisungjune <chltjdwns88@gmail.com>
2021-12-18 03:53:05 +0900

    basic javascript_3set

---
+++ week_14_Boolean_comparision_condition/comparison.js
@@ -0,0 +1,10 @@
+console.log(1+1);
+
+console.log(1 == 1);
+console.log(1 == 2);
+
+console.log(1 > 2);
+console.log(1 < 2);
+
+console.log(1 === 1);  // 내용과 데이터타입까지 같은지 비교 ==보다 강한것
+console.log(1 === 2);
---
+++ week_14_Boolean_comparision_condition/condition.js
@@ -0,0 +1,5 @@
+if(true){
+  console.log("hi~~");
+}else{
+  console.log("no hi~~");
+}
