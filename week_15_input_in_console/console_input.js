4d38920642112433723b21ead9bf50466b43bfdc
choisungjune <chltjdwns88@gmail.com>
2021-12-18 04:19:14 +0900

    console_input

---
+++ week_15_input_in_console/console_input.js
@@ -0,0 +1,10 @@
+var args = process.argv;
+console.log(args);
+// console.log(args[2]);
+
+if(args[2] === '1'){
+  console.log("parameter!");
+}
+// 1라인 --> 노드.exe 위치
+// 2라인 --> js실행파일 위치
+// 3라인 --> 입력값
