fb9aa92d31b39130aa790a21ce935698011bf1a3
choisungjune <chltjdwns88@gmail.com>
2021-12-17 12:30:51 +0900

    variable basic

---
+++ week_6_javasciprt_syntax/Variable.js
@@ -0,0 +1,16 @@
+a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+// 주석사용
+var aowkda = 3;
+aowkda = 4;
+console.log(aowkda);
+
+var letter = 'asdkoopawkdpoawkda29kda-dkasodpasd'
+console.log(letter);
+
+
+var name = 'sungjune';
+var letter_1 = 'wow so cold ' + name + ' are you okay?';
+console.log(letter_1);
