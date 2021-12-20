1699d1587c922b90622e2e2e48ffeff3e8d1a260
choisungjune <chltjdwns88@gmail.com>
2021-12-20 19:27:34 +0900

    refactoring

---
+++ week_43_App_Template_Refactoring/main.js
@@ -0,0 +1,163 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+
+//refactoring
+var template = {
+  html:function(title, list, body, control){
+    return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${control}
+    ${body}
+  </body>
+  </html>
+  `;
+  },
+  list:function (filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i++;
+    }
+    list = list+'</ul>';
+    return list;
+  }
+}
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    console.log()
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('../week_43_App_Template', function(error, filelist){
+          var list = template.list(filelist);
+          var title = 'welcome';
+          var description = "hello Node.js";
+          var html = template.html(title, list, `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`);
+          response.writeHead(200);  //성공코드
+          response.end(html);
+        });
+      } else {
+        fs.readdir('../week_43_App_Template', function(error, filelist){
+          fs.readFile(`${queryData.id}`, 'utf8', function(err, data){
+            var title = queryData.id;
+            var list = template.list(filelist);
+            var html = template.html(title, list, `<h2>${title}</h2>${data}`,
+            `
+            <a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>
+            <form action="/delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+            </form>
+            `
+            );
+            response.writeHead(200);  //성공코드
+            response.end(html);
+            });
+        });
+      }
+    } else if (pathname === '/create'){
+        fs.readdir('../week_43_App_Template', function(error, filelist){
+          var list = template.list(filelist);
+          var title = 'WEB-CREATE';
+          var html = template.html(title, list, `
+            <form action="/create_process" method='post'>
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="des"></textarea>
+              </p>
+              <p><input type="submit"></p>
+            </form>
+            `, '');
+          response.writeHead(200);  //성공코드
+          response.end(html);
+        });
+    } else if(pathname === '/create_process'){
+      var body = ``;
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var des = post.description;
+        fs.writeFile(`./${title}`, des, 'utf8', function(err){
+          response.writeHead(302, {Location:`/?id=${title}`}); // 302 는 redirection
+          response.end();
+        });
+      });
+    } else if (pathname === '/update'){
+      fs.readdir('../week_43_App_Template', function(error, filelist){
+        fs.readFile(`${queryData.id}`, 'utf8', function(err, data){
+          var title = queryData.id;
+          var list = template.list(filelist);
+          var html = template.html(title, list,
+            `
+            <form action="/update_process" method='post'>
+              <input type="hidden" name="id" value="${title}" >
+              <p><input type="text" name="title" placeholder="title" value=${title}></p>
+              <p>
+                <textarea name="description" placeholder="des">${data}</textarea>
+              </p>
+              <p><input type="submit"></p>
+            </form>
+            `,
+            `
+            <a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>
+            `
+          );
+          response.writeHead(200);  //성공코드
+          response.end(html);
+          });
+      });
+    } else if (pathname === '/update_process'){
+      var body = ``;
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var des = post.description;
+        fs.rename(`./${id}`, `./${title}`, function(error){
+          fs.writeFile(`./${title}`, des, 'utf8', function(err){
+            response.writeHead(302, {Location:`/?id=${title}`}); // 302 는 redirection
+            response.end();
+        })
+        });
+      });
+    } else if (pathname === '/delete_process'){
+      var body = ``;
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`./${id}`, function(err){         // 파일삭제
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        });
+      });
+    } else {
+      response.writeHead(404); //에러코드
+      response.end("Not found");
+    }
+});
+app.listen(3000);
